e59b83e6f953d44a06de1d176c1202436750bad9
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-02-28 23:39:38 +0545

    add new line

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import HeaderTemplate from "./components/header";
 import Header from "./components/navigation";
 import SignPage from "./pages/signup";
+import LoginPage from "./pages/login";
 
 export default function DataHandler(){
     return(
         <>
           {/* <HeaderTemplate/>
           <Header/> */}
-          <SignPage/>
+          {/* <SignPage/> */}
+          <LoginPage/>
+
         </>
     )
 
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,53 @@
 import { useState } from "react";
 
+export default function LoginPage() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
 
-export default function LoginPage(){
-    const 
+  async function loginUser(e) {
+    e.preventDefault();
+
+    const response = await fetch("http://127.0.0.1:8000/api/login/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    });
+    if (!response.ok) {
+      alert("Respond not found !!");
+    }
+    alert("login sucessfully");
+  }
+
+  return (
+    <>
+      <div>
+        <div>
+          <h3>Login</h3>
+        </div>
+        <form action="" onSubmit={loginUser}>
+          <div>
+            <input type="email" placeholder="Email" />
+          </div>
+          <div>
+            <input
+              type={showPassword ? "hide" : "show"}
+              placeholder="password....."
+            />
+            <button
+              type={showPassword? "hide":"show"}
+              className="signup-show-btn"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
