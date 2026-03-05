0b822f6f2c768866b0d692a1b2ff46f39bf40540
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-03-05 09:46:18 +0545

    userRole

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
 # Create your models here.
+userRole=[
+    ('VENDOR','vendor'),
+    ('CUSTOMER','customer')
+]
 class signUpModel(AbstractUser):
     email=models.EmailField(max_length=255,unique=True)
-    role=models.CharField(choices='')
+    role=models.CharField(choices=[userRole],default='customer')
     is_active=models.BooleanField(default=False)
     privacy_policy=models.BooleanField(default=False)
    
