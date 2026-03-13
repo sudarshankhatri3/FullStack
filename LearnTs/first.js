ace2c8616a68ff1391dacc2454b8652e535a9dc6
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-03-13 17:57:38 +0545

    learn Ts

---
+++ LearnTs/first.js
@@ -0,0 +1,5 @@
+var firstName = "blue";
+var lastName = "keto";
+var age = 234;
+var desc = "hey iam sudarshan khatri ";
+console.log("Hey iam ".concat(firstName, " ").concat(lastName, " and age is ").concat(age, " with description ").concat(desc));
--- LearnTs/first.ts
+++ LearnTs/first.ts
@@ -1,1 +1,5 @@
-console.log("hello iam sudarshan khatri")
+let firstName:String="blue"
+let lastName:String="keto"
+let age:Number=234
+let desc:String="hey iam sudarshan khatri "
+console.log(`Hey iam ${firstName} ${lastName} and age is ${age} with description ${desc}`);
