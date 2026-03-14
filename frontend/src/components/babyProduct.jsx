c6332ebe36a44c88684863c3294a5f65d629dc56
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-03-14 16:05:58 +0545

    add css

--- frontend/src/components/babyProduct.jsx
+++ frontend/src/components/babyProduct.jsx
@@ -1,52 +1,72 @@
 import "../app.css";
 
 export default function BabyProduct() {
+  const images = [
+    { img: "./newArrival.png", title: "New Arrival" },
+    { img: "./sales.png", title: "Clearance" },
+    { img: "./pregnancy.png", title: "Pregnancy & Postpartum" },
+    { img: "./milk.png", title: "Milks and Feeds" },
+    { img: "./diper.png", title: "Dipers and Wipes" },
+    { img: "./infant.png", title: "Infant" },
+    { img: "./eat.png", title: "Eat & Drink supplies" },
+    { img: "./stroller.png", title: "Stroller crib chair" },
+    { img: "./washes.png", title: "Washes & Bath" },
+    { img: "./cloth.png", title: "Baby Fashion" },
+  ];
 
-  const images=[
-    {img:"./newArrival.png",title:"New Arrival"},
-    {img:"./sales.png" ,title:"Clearance"},
-    {img:"./pregnancy.png",title:"Pregnancy & Postpartum"},
-    {img:"./milk.png",title:"Milks and Feeds"},
-    {img:"./diper.png",title:"Dipers and Wipes"},
-    {img:"./infant.png",title:"Infant"},
-    {img:"./eat.png",title:"Eat & Drink supplies"},
-    {img:"./stroller.png",title:"Stroller crib chair"},
-    {img:"./washes.png",title:"Washes & Bath"},
-    {img:"./cloth.png",title:"Baby Fashion"}
-  ]
   return (
     <>
-      <div className="mt-20 bg-[#FFFFFF] flex flex-col justify-center items-center gap-10 ">
+      <div className="mt-6 p-6 bg-[#FFFFFF] flex flex-col justify-center items-center gap-24">
         <div className="text-[#01A49E]">
-          <h2 className="text-4xl">Most popular categories <span className="text-[#000000]">for baby Product</span></h2>
+          <h2 className="text-4xl">
+            Most popular categories{" "}
+            <span className="text-[#000000]">for baby Product</span>
+          </h2>
         </div>
-        <div className="w-full flex flex-wrap justify-center gap-8">
-         {
-            images.map((prod,index)=>(
-                <div key={index} className="flex flex-col justify-center items-center gap-2 w-20 sm:w-20 md:w-30 ">
-                    <img src={prod.img}alt={prod.title} className="w-20  h-20 object-fit sm:w-20 sm:h-25 md:w-30 md:h-30"/>
-                    <p className="font-bold block text-xs ">{prod.title}</p>
-                </div>
-            ))
-         }
+        <div className="w-full flex  justify-center gap-8  ">
+          {images.map((prod, index) => (
+            <div
+              key={index}
+              className="flex flex-col justify-center items-center gap-2 w-20 sm:w-20 md:w-30 "
+            >
+              <img
+                src={prod.img}
+                alt={prod.title}
+                className="w-20  h-20 object-fit  sm:w-20 sm:h-25 md:w-30 md:h-30"
+              />
+              <p className="font-bold block text-xs ">{prod.title}</p>
+            </div>
+          ))}
         </div>
-        <div className="bg-centergrid grid-cols-2 gap-40 ">
-            <div className="bg-[url('/banner1.png')] bg-cover w-full h-[400]">
-                    <p>Best Price</p>
-                    <p>$69</p>
-                    <p>3-pack Cutton Rib Bodysuits</p>
-                    <button>Shop Now</button>
+        <div className="flex flex-row justify-evenly items-center gap-40 ">
+          <div className="bg-[url('/banner1.png')] bg-cover w-120 min-h-40">
+            <div className="flex flex-row items-center gap-10">
+              <p className="px-5 font-light text-2xl text-amber-100">
+                Best <br />
+                Price
+              </p>
+              <p className="mt-10 font-bold text-3xl text-amber-100">$69</p>
             </div>
-            <div className="bg-[url('/banner2.png')] bg-cover ">
-                
-                    <p>Best Price</p>
-                    <p>$69</p>
-                    <p>3-pack Cutton Rib Bodysuits</p>
-                    <button>Shop Now</button>
-                </div>
-                
-            
+            <p className="text-amber-50 text-light text-xl">
+              3-pack Cutton Rib <br />
+              Bodysuits
+            </p>
+            <button className="bg-amber-300 h-28 w-28 rounded-xl">
+              Shop Now
+            </button>
+          </div>
+          <div className="bg-[url('/banner2.png')] bg-cover min-w-120 min-h-40 ">
+            <div className="mt-80 flex flex-col  justify-end items-end  gap-10">
+              <p className="px-5 font-light text-2xl ">
+                10% OFF
+                <br />
+                for Diapers
+              </p>
+              <p className=" text-light text-xl block">
+                Earn 10% Cash back <br />on Swatbabymall. Expore Now!
+              </p>
             </div>
+          </div>
         </div>
       </div>
     </>
