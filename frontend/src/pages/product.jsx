839e19f4d9752bc956d58b067de52e2e631fa2cc
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-03-27 19:13:22 +0545

    product cart

--- frontend/src/pages/product.jsx
+++ frontend/src/pages/product.jsx
@@ -7,7 +7,7 @@ function Product() {
 
   async function products() {
     try {
-      const response = await fetch("http://127.0.0.1:8000/ecommerceApi/product/");
+      const response = await fetch("https://fakestoreapi.com/products/");
       if (!response.ok) {
         const errorText = await response.text();
         console.log(errorText);
@@ -29,22 +29,22 @@ function Product() {
   return (
     <>
     <h3>products list </h3>
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-50">
+    <div className="bg-amber-200  mt-5 p-10 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-50">
         
       {product.map((prod) => (
         <div
           key={prod.id}
-          className="bg-white p-5 rounded-xl shadow-sm"
+          className="bg-blue-800 w-50  h-70  border-b-blue-600 rounded-xl shadow-sm flex flex-col items-center gap-4"
         >
           <img
             src={prod.image}
             alt={prod.title}
-            className="w-full h-88 object-cover object-top drop-shadow-[0_80px_30px_#000]"
+            className="w-30 h-30  object-cover object-top drop-shadow-[0_80px_30px_#000]"
           />
-          <h3 className="text-2xl py-3 text-center font-medium">{prod.title}</h3>
-          <div className="m-30 flex justify-between items-center">
-            <p>$<span className="text-2xl font-medium">{prod.price}</span></p>
-            <button className="bg-gray-300 p-3 rounded-md text-sm hover:bg-gray-400 flex gap-3">Buy</button>
+          <h3 className="text-[10px] py-3 text-center font-medium">{prod.title}</h3>
+          <div className=" flex justify-between items-center gap-10">
+           
+            <button className="bg-gray-300 p-3 w-1 rounded-md text-sm hover:bg-gray-400 flex gap-3">Buy</button>
             <button className="bg-gray-300 p-3 rounded-md text-sm hover:bg-gray-400 flex gap-3" onClick={()=>setCart(cart+1)}>Add To Cart</button>
           </div>
         
